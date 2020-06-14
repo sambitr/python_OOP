ea9fe7d3799209f4c96382f235c09a310245f77e
sambitr <kumar.sambit7@gmail.com>
2020-06-14 21:51:52 +0530

    initial commit

--- Employee.py
+++ Employee.py
@@ -24,3 +24,10 @@ print(emp2.displayEmployeeDetails())
 emp1.name = "Rahul"
 print(emp1.displayEmployeeDetails())
 
+##Built-In Class Attributes
+print("Employee.__doc__:", Employee.__doc__)
+print("Employee.__name__:", Employee.__name__)
+print("Employee.__module__:", Employee.__module__)
+print("Employee.__bases__:", Employee.__bases__)
+print("Employee.__dict__:", Employee.__dict__)
+
